1570cd71401ced50dda5368bb3d59c115cbdd67a
skynet11 <jmcintire97@gmail.com>
2015-12-10 16:32:12 -0500

    added thumbnails, updated JSON file

---
+++ JSON-Files/wallpapers.js
@@ -0,0 +1,168 @@
+{
+	"wallpapers": {
+		
+		"category":[
+			{
+				"name": "Neonique"
+				"wallpaper":[
+					{
+						"author": "Customworx",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/customworx1.jpg",
+						"thumbUrl": https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/customworx1-thumb.jpg",
+						"name": "Customworx1"
+					},
+					{
+						"author": "Customworx",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/customworx2.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/customworx2-thumb.jpg",
+						"name": "Customworx2"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/abstract1.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/abstract1-thumb.jpg",
+						"name": "abstract1"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/abstract2.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/abstract2-thumb.jpg",
+						"name": "abstract2"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/abstract3.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/abstract3-thumb.jpg",
+						"name": "abstract3"					
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/abstract4.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/abstract4-thumb.jpg",
+						"name": "abstract4"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/abstract5.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/abstract5-thumb.jpg",
+						"name": "abstract5"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/abstract6.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/abstract6-thumb.jpg",
+						"name": "abstract6"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/abstract7.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/abstract7-thumb.jpg",
+						"name": "abstract7"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/abstract8.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/abstract8-thumb.jpg",
+						"name": "abstract8"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/cutout1.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/cutout1-thumb.jpg",
+						"name": "cutout1"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/cutout2.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/cutout2-thumb.jpg",
+						"name": "cutout2"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/cutout3.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/cutout3-thumb.jpg",
+						"name": "cutout3"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/cutout4.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/cutout4-thumb.jpg",
+						"name": "cutout4"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/cutout5.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/cutout5-thumb.jpg",
+						"name": "cutout5"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/cutout6.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/cutout6-thumb.jpg",
+						"name": "cutout6"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/cutout7.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/cutout7-thumb.jpg",
+						"name": "cutout7"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/cutout8.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/cutout8-thumb.jpg",
+						"name": "cutout8"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/scenic1.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/scenic1-thumb.jpg",
+						"name": "scenic1"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/scenic2.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/scenic2-thumb.jpg",
+						"name": "scenic2"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/scenic3.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/scenic3-thumb.jpg",
+						"name": "scenic3"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/scenic4.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/scenic4-thumb.jpg",
+						"name": "scenic4"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/scenic5.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/scenic5-thumb.jpg",
+						"name": "scenic5"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/scenic6.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/scenic6-thumb.jpg",
+						"name": "scenic6"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/scenic7.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/scenic7-thumb.jpg",
+						"name": "scenic7"
+					},
+					{
+						"author": "Jeff McIntire",
+						"url": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/scenic8.jpg",
+						"thumbUrl": "https://raw.githubusercontent.com/skynet11/Neonique-walls/master/Wallpapers/scenic8-thumb.jpg",
+						"name": "scenic8"
+					}
+				]		
+			}		
+		]
+	}
+}
